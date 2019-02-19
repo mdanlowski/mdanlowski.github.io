1acf485d5b833640039c81af6896aa7720c090a6
mdanlowski <marcindanlowski@gmail.com>
2019-02-19 16:56:38 +0100

    temp disable of bottom sections and adjustments

--- assets/main.js
+++ assets/main.js
@@ -55,29 +55,27 @@ document.addEventListener('keydown', function(e){
 	console.log(e.code);
 	if (e.code == 'Space') document.getElementById('TASC').click();
 	else if (e.code == 'ArrowUp'){
-		
-		if (window.location.href.endsWith('.io') || window.location.href.includes('#projects-section')) {
+		//if (window.location.href.includes('.io') || window.location.href.includes('.io') || window.location.href.includes('#projects-section')) {
 			//pass
-		}//1 ^
-		if(window.location.href.includes('#cv-section')){
+		//}//1 ^
+		if(window.location.href.endsWith('#cv-section')){
 			document.querySelector('a[href="#projects-section"]').click();
 		}//2 ^
-		else if (window.location.href.includes('#about-section')) {
+		if (window.location.href.endsWith('#about-section')) {
 			document.querySelector('a[href="#cv-section"]').click();
 		}//3 ^
 		
 	}
 	else if (e.code == 'ArrowDown') {
-		
-		if (window.location.href.includes('.io') || window.location.href.includes('.html') || window.location.href.includes('#projects-section')) {
+		if(window.location.href.endsWith('.io') || window.location.href.endsWith('.html') || window.location.href.endsWith('#projects-section')) {
 			document.querySelector('a[href="#cv-section"]').click();
 		}//1 v
-		else if(window.location.href.includes('#cv-section')){
+		else if(window.location.href.endsWith('#cv-section')){
 			document.querySelector('a[href="#about-section"]').click();
 		}//2 v
-		else if (window.location.href.includes('#about-section')) {
-			document.querySelector('a[href="#@TODO-SECTION"]').click();
-		}//3 v
+		//if (window.location.href.includes('#about-section')) {
+			//document.querySelector('a[href="#@TODO-SECTION"]').click();
+		//}//3 v
 		
 	}
 });
