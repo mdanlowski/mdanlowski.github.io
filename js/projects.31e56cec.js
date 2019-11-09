5fd4abb3fef21c16b7a7fb3b549636be335c3581
mdanlowski <marcindanlowski@gmail.com>
2019-11-10 00:18:27 +0100

    NEW PORTFOLIO USING VUE

---
+++ js/projects.31e56cec.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["projects"],{acca:function(e,t,a){"use strict";a.r(t);var n=function(){var e=this,t=e.$createElement,a=e._self._c||t;return a("div",{staticClass:"animated fadeIn",attrs:{id:"projects"}},[a("div"),a("div",[a("projects-gallery")],1)])},r=[],s=a("3b24"),c={name:"projects",components:{ProjectsGallery:s["a"]},data(){return{}}},o=c,d=a("2877"),i=Object(d["a"])(o,n,r,!1,null,"7db13de2",null);t["default"]=i.exports}}]);
+//# sourceMappingURL=projects.31e56cec.js.map
--- rts/PlayerProjectile.js
+++
@@ -1,5 +0,0 @@
-// Blueprint for bullets that the Player shoots out
-
-function PlayerProjectile(x_, y_, vel_){
-	
-}
