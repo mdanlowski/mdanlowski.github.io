ec14a5af23cab4c1759793bf52f6be7846a167d2
mdanlowski <marcindanlowski@gmail.com>
2020-10-23 05:15:50 +0200

    FLOW TEST revert later

--- flow/kj/js/main.js
+++ flow/kj/js/main.js
@@ -1,28 +1,41 @@
+var menuShown = false;
+
 document.addEventListener('DOMContentLoaded', initialize);
 
 function initialize() {
-
   const menuButton = document.getElementById('menu-button');
+  const overlay = document.getElementById('overlay');
   menuButton.addEventListener('click', menuToggle);
+  overlay.addEventListener('click', menuHide);
 
 }
 
 function menuToggle() {
-  console.log("MENU KLIKNIĘTE");
-
   const menu = document.getElementById('menu-expanded');
   const overlay = document.getElementById('overlay');
-  
-  menu.style.left = "calc(100% - 560px)";
-  overlay.style.display = "initial";
+  const svgBurger = document.querySelector('g.open');
+  const svgCross = document.querySelector('g.close');
 
-  overlay.addEventListener('click', menuHide);
+  if(!menuShown){
+    menu.style.right = "0";
+    overlay.style.display = "initial";
+
+    svgBurger.style.opacity = "0";
+    svgCross.style.opacity = "1";
+
+  } else {
+    menu.style.right = "-100%";
+    overlay.style.display = "none";
+
+    svgBurger.style.opacity = "1";
+    svgCross.style.opacity = "0";
+  }
+  menuShown = !menuShown;
+
 }
 
-function menuHide() {
-  const menu = document.getElementById('menu-expanded');
-  const overlay = document.getElementById('overlay');
 
-  menu.style.left = "calc(100%)";
-  overlay.style.display = "none";
+function menuHide() {
+  menuShown = true;
+  return menuToggle();
 }
